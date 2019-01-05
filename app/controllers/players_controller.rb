97aab4f867a1f2a803b4d9607c52c5f753240df2
Marcio Toshio <marcio@tosh.io>
2019-01-05 00:25:50 -0200

    player card with no link

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,7 @@ class PlayersController < ApplicationController
   def show
     game = find_game(params[:slug])
     player = find_player(game, params[:id])
+    @no_link = true
     render partial: 'show', locals: { player: player }
   end
 end
